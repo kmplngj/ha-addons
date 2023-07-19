b95ec3287a8b892ddfa99e5719e0f652023ecbff
Jan Kampling <562965+kmplngj@users.noreply.github.com>
2023-07-19 14:09:23 +0200

    added auto config for pixoo ip

--- pixoo-rest/run.sh
+++ pixoo-rest/run.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
 
 set -e
 
 bashio::log.info "Reading Config from Home Asssitant Addon..."
-export PIXOO_HOST=$(bashio::config 'PIXOO_HOST')
-export PIXOO_DEBUG=$(bashio::config 'PIXOO_DEBUG') 
-export PIXOO_SCREEN_SIZE=$(bashio::config 'PIXOO_SCREEN_SIZE')
+PIXOO_HOST_AUTO=$(bashio::config 'PIXOO_HOST_AUTO')
+PIXOO_DEBUG=$(bashio::config 'PIXOO_DEBUG') 
+export PIXOO_DEBUG
+PIXOO_SCREEN_SIZE=$(bashio::config 'PIXOO_SCREEN_SIZE')
+export PIXOO_SCREEN_SIZE
+
+if [ $PIXOO_HOST_AUTO = True ] 
+then
+	PIXOO_HOST=$(curl -X POST "https://app.divoom-gz.com/Device/ReturnSameLANDevice" | jq .DeviceList[0].DevicePrivateIP)
+else
+	PIXOO_HOST=$(bashio::config 'PIXOO_HOST')
+fi
+export PIXOO_HOST
 
 # Start Pixoo Rest server
 bashio::log.info "Starting Pixoo-Rest server..."
